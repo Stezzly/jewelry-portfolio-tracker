b5bad08321fa041f2525bb0349e1ea956a4881c2
Stezzly <43850376+Stezzly@users.noreply.github.com>
2025-02-03 16:39:27 -0330

    Create react app using tailwind CSS
    - Create jewelry-portfolio-tracker react app using tail CSS

---
+++ jewelry-portfolio-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JewelryPortfolio from './components/JewelryPortfolio'; 
+import './input.css'; 
+
+function App() {
+  return (
+    <div className="App"> {}
+      <JewelryPortfolio />
+    </div>
+  );
+}
+
+export default App;
